e452cfd850af3fab609e7b1ac3ab7e292a61d04d
gagewisneski <gagewisneski@gmail.com>
2017-02-09 11:21:01 -0800

    toggle hamburger-menu is-open display

--- app/assets/javascripts/hamburger_menu.js
+++ app/assets/javascripts/hamburger_menu.js
@@ -3,26 +3,38 @@ $(document).ready(function () {
       overlay = $('.overlay'),
      isClosed = false;
 
-    trigger.click(function () {
-      hamburger_cross();      
-    });
+    // trigger.click(function () {
+    //   hamburger_cross();      
+    // });
 
-    function hamburger_cross() {
+    // function hamburger_cross() {
 
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
+    //   if (isClosed == true) {          
+    //     overlay.hide();
+    //     trigger.removeClass('is-open');
+    //     trigger.addClass('is-closed');
+    //     isClosed = false;
+    //   } else {   
+    //     overlay.show();
+    //     trigger.removeClass('is-closed');
+    //     trigger.addClass('is-open');
+    //     isClosed = true;
+    //   }
+    // }
+
+    // trigger.click(function () {
+    //   hamburger_cross();
+    // });
   
   $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
+      $('#wrapper').toggleClass('toggled');
+      var button = document.getElementsByClassName("hamburger");
+      for (var i = 0; i < button.length; i++) {
+        if (button[i].style.display === 'none') {
+          button[i].style.display = '';
+        } else {
+          button[i].style.display = 'none';
+        }
+      }
   });  
 });
