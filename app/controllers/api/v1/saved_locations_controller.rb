34524f7b109837590f4ed8827de25eef57f3ef1e
meg-murphy <meghan.murphy.usc@gmail.com>
2017-02-15 18:34:36 -0800

    add google icons for heart icon to save location, add update controller

--- app/controllers/api/v1/saved_locations_controller.rb
+++ app/controllers/api/v1/saved_locations_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::SavedLocationsController < ApplicationController
+  
+  def create
+    @location = Location.find_by(location_params)
+    if @location.save
+      render json: @location
+    else
+
+    end
+  end
+
 end
