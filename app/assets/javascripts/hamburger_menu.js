a762570f793b3428a72ad16fb28aa2cfb29aa3f4
gagewisneski <gagewisneski@gmail.com>
2017-02-18 10:17:21 -0800

    mobile: button stuck on the bottom. sm and lg: button not stuck to the bottom.

--- app/assets/javascripts/hamburger_menu.js
+++ app/assets/javascripts/hamburger_menu.js
@@ -2,19 +2,20 @@ $(document).ready(function () {
   var trigger = $('.hamburger'),
       overlay = $('.overlay'),
      isClosed = false;
+      $window = $(window);
 
     // trigger.click(function () {
-    //   hamburger_cross();      
+    //   hamburger_cross();
     // });
 
     // function hamburger_cross() {
 
-    //   if (isClosed == true) {          
+    //   if (isClosed == true) {
     //     overlay.hide();
     //     trigger.removeClass('is-open');
     //     trigger.addClass('is-closed');
     //     isClosed = false;
-    //   } else {   
+    //   } else {
     //     overlay.show();
     //     trigger.removeClass('is-closed');
     //     trigger.addClass('is-open');
@@ -25,10 +26,22 @@ $(document).ready(function () {
     // trigger.click(function () {
     //   hamburger_cross();
     // });
-  
+
+  function checkWidth() {
+    if ($window.width() > 576) {
+      $('#addLocationButton').removeClass('small-button').addClass('medium-button');
+    } else if ($window.width() <= 575) {
+      $('#addLocationButton').removeClass('medium-button').addClass('small-button');
+    };
+  }
+
+  checkWidth();
+  $(window).resize(checkWidth);
+
   $('[data-toggle="offcanvas"]').click(function () {
       $('#wrapper').toggleClass('toggled');
       var button = document.getElementsByClassName("hamburger");
+      var addLocation = document.getElementById("addLocationButton");
       for (var i = 0; i < button.length; i++) {
         if (button[i].style.display === 'none') {
           button[i].style.display = '';
@@ -36,5 +49,10 @@ $(document).ready(function () {
           button[i].style.display = 'none';
         }
       }
-  });  
+      if (addLocation.style.display === 'none') {
+        addLocation.style.display = '';
+      } else {
+        addLocation.style.display = 'none';
+      }
+  });
 });
