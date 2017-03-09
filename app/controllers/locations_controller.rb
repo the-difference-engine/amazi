fe6cd961696b1de9edd204dd30a0aaddf88b04d6
gagewisneski <gagewisneski@gmail.com>
2017-03-08 19:23:49 -0800

    working on carousel

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -10,13 +10,12 @@ class LocationsController < ApplicationController
     @image = Image.new
 
     @allpictures = []
-    @c.instance_variable_get(:@photos).each do |pic| 
-    @allpictures << (pic.fetch_url(350)) 
-    end 
-    @images.each do |image| 
-      @allpictures << image.image.url 
-    end 
-
+    @c.instance_variable_get(:@photos).each do |pic|
+      @allpictures << (pic.fetch_url(350))
+    end
+    @images.each do |image|
+      @allpictures << image.image.url
+    end
   end
 
   def map
