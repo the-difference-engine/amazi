abda6c14d44f98581a4be6e836a847cd50b76beb
gagewisneski <gagewisneski@gmail.com>
2017-03-13 19:47:46 -0700

    working on sharing a link on location show page

---
+++ app/assets/javascripts/location_show.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+  function shareButton() {
+    var share = document.getElementById("share-well");
+    if (share.style.display === 'none') {
+      share.style.display = '';
+    }
+});
