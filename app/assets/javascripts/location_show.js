4cb854fcf19552c4d896899469fd0ef13ca1f943
gagewisneski <gagewisneski@gmail.com>
2017-03-13 20:44:34 -0700

    javascript pops-up link to copy

--- app/assets/javascripts/location_show.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-
-  function shareButton() {
-    var share = document.getElementById("share-well");
-    if (share.style.display === 'none') {
-      share.style.display = '';
-    }
-});
