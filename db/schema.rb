c9b188f92cf901faaed8b6959c393134f9a811ec
gagewisneski <gagewisneski@gmail.com>
2017-03-26 14:21:18 -0700

    styling add-location and fixed form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323013206) do
+ActiveRecord::Schema.define(version: 20170324230819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,11 +107,6 @@ ActiveRecord::Schema.define(version: 20170323013206) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "google_place"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-    t.string   "water_type"
   end
 
   create_table "problems", force: :cascade do |t|
