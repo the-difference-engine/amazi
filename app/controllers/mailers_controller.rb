4b7365642e37caec54dc8955a75599eb2093cfb6
Jake Rabeck <Jakerabeck@gmail.com>
2017-05-15 19:06:54 -0700

    mailer done

--- app/controllers/mailers_controller.rb
+++ app/controllers/mailers_controller.rb
@@ -3,8 +3,7 @@ class MailersController < ApplicationController
 	def send_feedback
 		message = params[:message]
 		email = params[:email]
-		FeedbackMailer.feedback_email(message).deliver_now
-		
+		FeedbackMailer.feedback_email(message, email).deliver_now
 		redirect_to "#"
 	end
 
--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,9 +1,10 @@
 class FeedbackMailer < ApplicationMailer
 
-  def feedback_email(message)
+  def feedback_email(message, email)
 
 	  @message = message
-	  mail(to: "jakerabeck@gmail.com", subject: 'Amazi User Feedback') do |format|
+	  @email = email
+	  mail(to: "theamaziapp@gmail.com", subject: 'Amazi User Feedback') do |format|
 			format.html { render 'feedback_email.html.erb' }
 			end
 
